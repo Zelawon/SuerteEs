44a635e885d385d2bee8ea67beda39a1224a45d2
tsami-mar <marilenatsami@gmail.com>
2024-11-15 09:58:37 +0100

    added on failure listener when location access is not granted

--- app/src/main/java/dte/masteriot/mdp/suertees/offices/OfficesActivity.java
+++ app/src/main/java/dte/masteriot/mdp/suertees/offices/OfficesActivity.java
@@ -13,7 +13,6 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -122,6 +121,12 @@ public class OfficesActivity extends AppCompatActivity implements OnDataLoadedLi
                                 // Call sortItemsByDistance once location is available
                                 sortItemsByDistance();
                             }
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(OfficesActivity.this, "Failed to get location: " + e.getMessage(), Toast.LENGTH_SHORT).show();
                         }
                     });
         }
